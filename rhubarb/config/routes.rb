e33ca97d8206b33eb7575705e2da4eb0b83e71f9
JoeyShapiro <joeyashapiro@gmail.com>
2023-07-04 18:05:15 -0500

    added api call

--- rhubarb/config/routes.rb
+++ rhubarb/config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/hello", to: "hello#index"
+  # better to do this instead of subdomain and multiple views with inheretance
+  # get multiple in one part
+  # should use sub stuff if this does have api (it will)
   get "/api/:call", to: "api#index", constraints: { call: /.*/ }
   constraints subdomain: 'app' do
-    
-  end
-
-  constraints subdomain: 'api' do
-    # get "/test/:call", to: "hello#test"
     get "/test/:call", to: "hello#index", constraints: { call: /.*/ }
   end
 end
